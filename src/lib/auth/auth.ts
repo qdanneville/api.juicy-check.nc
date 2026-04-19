e9e09c135a14cad89422fcabea63393122083e67
qdanneville <quentin@esperluette.io>
2026-04-20 09:18:29 +1100

    [Auth] added

--- src/lib/auth/auth.ts
+++ src/lib/auth/auth.ts
@@ -1,8 +1,11 @@
+import 'dotenv/config';
+
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { db } from "../db/drizzle"; // your drizzle instance
 
 export const auth = betterAuth({
+    secret: process.env.BETTER_AUTH_SECRET,
     trustedOrigins: [process.env.FRONTEND_URL ?? '*'],
     database: drizzleAdapter(db, {
         provider: "pg",
